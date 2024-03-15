c86c6ea598dcac91d4177c88870c56f00c108432
NgocNguyen2503 <ngocnq.ba12-139@st.usth.edu.vn>
2024-03-15 22:17:52 +0700

    Update file input.py

--- pw5/input.py
+++ pw5/input.py
@@ -2,7 +2,7 @@ from domain.student import Student
 from domain.course import Course
 from output import show_courses, show_students
 import math
-import gzip
+import zlib
 
 def number_students():
     st_number = int(input("Number of student: "))
@@ -26,28 +26,6 @@ def input_course():
     name = input("Name: ")
     return Course(id,name)
 
-'''
-def show_students(students_list):
-    print("------------------------------------")
-    if len(students_list) == 0:     # if the list is empty
-        print("The student list is empty")
-    else:
-        print("Student list:")
-        for i in range(len(students_list)):
-            print(f"| ID: {students_list[i].getId()} | FULL NAME: {students_list[i].getName()} | DATE OF BIRTH: {students_list[i].getDob()} |")
-    print("------------------------------------")
-
-
-def show_courses(courses_list):
-    print("------------------------------------")
-    if len(courses_list) == 0:      # if the list is empty
-        print("The course list is empty")
-    else:
-        print("Course list: ")
-        for i in range(len(courses_list)):
-            print(f"INDEX: {i + 1} | COURSE ID: {courses_list[i].getId()} | COURSE NAME: {courses_list[i].getName()}")
-    print("------------------------------------")
-'''
 
 def assign_students(students_list):
     print("input assigned student: ")
@@ -114,47 +92,109 @@ def input_mark(course_list,course_student):
                         print(f"The ID {st_input} does not exist")
 
 
-def write_student(student_list):
-    f = open("Student.txt","w")
-    for i in student_list:
-        f.write(f"| ID: {i.getId()} | FULL NAME: {i.getName()} | DoB: {i.getDob()}" + "\n")
+def write_students(student_list):
+    with open("students.txt", "w") as f:
+        for i,student in enumerate(student_list):
+            if i == (len(student_list)-1):
+                f.write(f"{student.getId()},{student.getName()},{student.getDob()}")
+            else:
+                f.write(f"{student.getId()},{student.getName()},{student.getDob()}\n")
+        f.write("!")
     f.close()
 
-def write_course(course_list):
-    f = open("Course.txt","w")
-    for i in course_list:
-        f.write(f"| ID: {i.getId()} | NAME: {i.getName()} |" + "\n")
+def write_courses(course_list):
+    with open("courses.txt", "w") as f:
+        for i,course in enumerate(course_list):
+            if i == (len(course_list)-1):
+                f.write(f"{course.getId()},{course.getName()}")
+            else:
+                f.write(f"{course.getId()},{course.getName()}\n")
+        f.write("!")
     f.close()
-
+'''
 def write_mark(student_list):
-    f = open("Mark.txt","w")
-    for i in student_list:
-        f.write(f"| ID: {i.getId()} | FULL NAME: {i.getName()}")
-        for course, mark in i.mark.items():
-            f.write(f" | Course: {course} | Mark: {mark} |\n")
+    f = open("marks.txt","w")
+    for i,student in enumerate(student_list):
+        if i == (len(student_list)-1):
+            f.write(f"|{student.getId()}|{student.getName()}")
+            for course, mark in student.mark.items():
+                f.write(f"|{course}|{mark}")
+        else:
+            f.write(f"|{student.getId()}|{student.getName()}")
+            for course, mark in student.mark.items():
+                f.write(f"|{course}|{mark}"+"\n")
     f.close()
+'''
+def write_mark(student_list):
+    with open("marks.txt", "w") as f:
+        for i, student in enumerate(student_list):
+            f.write(f"{student.getId()},{student.getName()}")
+            for course, mark in student.mark.items():
+                f.write(f",{course},{mark}")
+            if i < len(student_list) - 1:
+                f.write("\n")
 
-def compress_infor():
-    with gzip.open("Student.zip","wb") as f:
-        student = open("Student.txt","rb")
-        f.write(student.read())
-        course = open("Course.txt","rb")
-        f.write(course.read())
-        mark = open("Mark.txt","rb")
-        f.write(mark.read())
+def compress_files():
+        with open("students.txt", "rb") as f:
+            students_data = f.read()
+        with open("courses.txt", "rb") as f:
+            courses_data = f.read()
+        with open("marks.txt", "rb") as f:
+            marks_data = f.read()
+
+        compressed_data = zlib.compress(students_data + courses_data + marks_data)
+
+        with open("students.dat", "wb") as f:
+            f.write(compressed_data)
+
+def decompress_files(student_list,course_list,course_student):
+
+    with open("students.dat", "rb") as f:
+        compressed_data = f.read()
+
+    decompressed_data = zlib.decompress(compressed_data)
+
+    convert_data = decompressed_data.decode()  # Convert from bytes to string
+    x=convert_data.split("!")
+    students,courses,marks = x
+    with open("students.txt", "w") as f:
+        f.write(students)
+        lines = students.split("\n")
+        for i in lines:
+           data = i.split(",")
+           st_id,name,dob = data
+           student_list.append(Student(st_id,name,dob))
+        
+    with open("courses.txt", "w") as f:
+        f.write(courses)
+        lines = courses.split("\n")
+        for i in lines:
+           data = i.split(",")
+           cs_id,name = data
+           course_list.append(Course(cs_id,name))
+
+    with open("marks.txt", "w") as f:
+        f.write(marks)
+        lines = marks.split("\n")
+        for i in lines:
+            data = i.split(",")
+            st_id, name, *mark_list = data
+            print(f"a + {mark_list}")
+            for student in student_list:
+                if st_id == student.getId():
+                    for j in range(0, len(mark_list), 2):
+                        course = mark_list[j]
+                        mark = mark_list[j + 1]
+                        student.mark[course] = mark
+                    
 
 
-def decompress_infor():
-    try:
-        with gzip.open("Student.zip","rb") as f:
-            student = open("Student.txt","wb")
-            student.write(f.read())
-            course = open("Course.txt","wb")
-            course.write(f.read())
-            mark = open("Mark.txt","wb")
-            mark.write(f.read())
-    except FileNotFoundError:
-        print("compressed file does not exist")
+
+
+
+    
+
+
 
                 
                 
